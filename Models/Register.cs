917e5ff99f67d04dbad930e5022c60cccf2539d0
LiaDwiyanti <alixy675@email.com>
2025-06-14 17:35:55 +0700

    Update Fitur Auth

--- Models/Register.cs
+++ Models/Register.cs
@@ -1,10 +1,14 @@
-﻿namespace APITaklimSmart.Models
+﻿using System.Text.Json.Serialization;
+
+namespace APITaklimSmart.Models
 {
     public class Register
     {
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
+        
+        [JsonPropertyName("no_hp")]
         public string No_hp { get; set; }
         public string Alamat { get; set; }
 
