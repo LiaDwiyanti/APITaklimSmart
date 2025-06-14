727c5f999aafbf050f8dba76b22a3b038c51aed6
LiaDwiyanti <alixy675@email.com>
2025-06-14 14:51:22 +0700

    Update

--- Helpers/DBHelper.cs
+++ Helpers/DBHelper.cs
@@ -6,6 +6,7 @@ namespace APITaklimSmart.Helpers
     public class DBHelper
     {
         private NpgsqlConnection connection;
+        private NpgsqlTransaction transaction;
         private string __constr;
         public DBHelper(string pConstr)
         {
@@ -13,18 +14,43 @@ namespace APITaklimSmart.Helpers
             connection = new NpgsqlConnection(pConstr);
             connection.ConnectionString = __constr;
         }
-        public NpgsqlCommand GetNpgsqlCommand(string query)
+        public void BeginTransaction()
         {
-            connection.Open();
-            NpgsqlCommand cmd = new NpgsqlCommand();
-            cmd.Connection = connection;
-            cmd.CommandText = query;
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+            transaction = connection.BeginTransaction();
+        }
+
+        public void CommitTransaction()
+        {
+            transaction?.Commit();
+            CloseConnection();
+        }
+
+        public void RollbackTransaction()
+        {
+            transaction?.Rollback();
+            CloseConnection();
+        }
+        public NpgsqlCommand GetNpgsqlCommand(string query, bool useTransaction = false)
+        {
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+
+            NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+            if (useTransaction && transaction != null)
+                cmd.Transaction = transaction;
+
             cmd.CommandType = CommandType.Text;
             return cmd;
         }
-        public void closeConnection()
+        public void CloseConnection()
         {
-            connection.Close();
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
         }
     }
 }
