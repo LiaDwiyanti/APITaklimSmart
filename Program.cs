eb2830f07ffbf22c5ee5d98296785ff0001e351a
LiaDwiyanti <alixy675@email.com>
2025-06-14 12:00:43 +0700

    Update fitur-auth

--- Program.cs
+++ Program.cs
@@ -120,7 +120,6 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
     });
 }
 
-app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseCors("AllowMobileApp");
